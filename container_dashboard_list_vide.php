f599a8c9b06bad09dae65667a2d91a83e35fdd48
PCGameJK <j.kasperski@codeur.online>
2022-01-04 16:08:18 +0100

    newversion2

---
+++ container_dashboard_list_vide.php
@@ -0,0 +1,6 @@
+<?php
+echo "
+<div class=\"d-grid gap-2 col-3\">
+<a href=\"inserer.php\"><button type=\"button\" class=\"btn btn-light btn-lg position-absolute top-50 start-50 translate-middle border\"><img src=\"img\sound2.png\"> Ajouter</button></a>
+</div>
+";
