0d307e6e19a2996d748e349bb4b12af9695a4fb5
Mika73100 <Michael-73@live.Fr>
2022-01-12 17:37:54 +0100

    debut css

--- inscription.php
+++ inscription.php
@@ -5,14 +5,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
-    </script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="CSS\style.css">
-    <title>Inscription</title>
+    <link rel="stylesheet" href=".\gitebonbon.css">
 </head>
 
 <body class='main-bg'>
@@ -55,11 +49,10 @@
     }
     ?>
         <div class='container'>
-            <div class='row justify-content-center mt-5'>
+            <div class='row1'>
                 <div class='col-lg-4 col-md-8 col-sm-8'>
                     <div class='card shadow'>
                         <div class='card-title text-center border-bottom'>
-                            <h2 class='p-3'>Inscription</h2>
                         </div>
                         <div class='card-body'>
                             <form action='validationformulaire.php' method='post'>
@@ -89,7 +82,7 @@
                                     <label for='form-cgv'>J'ai lu les conditions d'utilisation</label>
                                 </div>
                                 <div class='d-grid'>
-                                    <button type='submit' class='btn main-bg text-light'>S'inscrire</button>
+                                    <button type='submit' class='btn-main-bg-text-light'>Inscription</button>
                                 </div>
                             </form>
                         </div>
@@ -100,8 +93,6 @@
                 </div>
             </div>
         </div>
-
-
 </body>
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
