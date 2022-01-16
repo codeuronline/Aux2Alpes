7752f33c07b47cbcf1f6f46f5313febb61c6ee1a
PCGameJK <j.kasperski@codeur.online>
2022-01-16 19:13:02 +0100

    back-dev21

--- dev/details.php
+++ dev/details.php
@@ -19,7 +19,6 @@ if (isset($_GET['id_hebergement']) && !empty($_GET['id_hebergement'])) {
 
     //trait les elmements de la table jours
     //on verifie si le hebergement existe
-    echo $hebergement['id_hebergement'];
     if (!$hebergement) {
         $_SESSION['erreur'] = "Cet ID n'existe pas";
         //header('Location: index.php');
@@ -36,6 +35,8 @@ if (isset($_GET['id_hebergement']) && !empty($_GET['id_hebergement'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="../gitebonbon.css">
+    <link rel="stylesheet" href="radio.css">
     <title>Détails de l'hébergement</title>
 </head>
 
@@ -43,34 +44,35 @@ if (isset($_GET['id_hebergement']) && !empty($_GET['id_hebergement'])) {
     <main class="container">
         <div class="row">
             <section class="col-12">
-                <h1>Détails de l'hébergement :<?= $hebergement['nom'] ?>
-                    <p>Id: <?= $hebergement['id_hebergement'] ?></p>
-                    <p>Categorie:<?= $hebergement['categorie'] ?></p>
-                    <p>Nom: <?= $hebergement['nom'] ?></p>
-                    <p>Adresse: <?= $hebergement['adresse'] ?></p>
-                    <p>Description: <?= $hebergement['description'] ?></p>
-                    <p>Prix:<?= $hebergement['prix'] ?></p>
-                    <p>Couchage:<?= $hebergement['couchage'] ?></p>
-                    <p>Sdb: <?= $hebergement['sdb'] ?></p>
-                    <p>
-                        <?= ($hebergement['animaux'] == 1) ? "<img src='../image/animauxpicto.png' width='50'>" : "<img src='../image/animauxpictorouge.png' width='50'>";  ?>
-                        <?= ($hebergement['wifi'] == 1) ? "<img src='../image/wifipicto.png' width='50'>" : "<img src='../image/wifipictorouge.png' width='50'>";  ?>
-                        <?= ($hebergement['fumeur'] == 1) ? "<img src='../image/fumeurpicto.png' width='50'>" : "<img src='../image/fumeurpictorouge.png' width='50'>";  ?>
-                        <?= ($hebergement['piscine'] == 1) ? "<img src='../image/piscinepicto.png' width='50'>" : "<img src='../image/piscinepictorouge.png' width='50'>";  ?>
-                    </p>
+                <h1>Détails de l'hébergement : <?= $hebergement['nom'] ?></h1>
+                <p>Id: <?= $hebergement['id_hebergement'] ?></p>
+                <p>Categorie:<?= $hebergement['categorie'] ?></p>
+                <p>Nom: <?= $hebergement['nom'] ?></p>
+                <p>Adresse: <?= $hebergement['adresse'] ?></p>
+                <p>Description: <?= $hebergement['description'] ?></p>
+                <p>Prix:<?= $hebergement['prix'] ?></p>
+                <p>Couchage:<?= $hebergement['couchage'] ?></p>
+                <p>Sdb: <?= $hebergement['sdb'] ?></p>
+                <p>
+                    <?= ($hebergement['animaux'] == 1) ? "<img src='../image/animauxpicto.png' width='50'>" : "<img src='../image/animauxpictorouge.png' width='50'>";  ?>
+                    <?= ($hebergement['wifi'] == 1) ? "<img src='../image/wifipicto.png' width='50'>" : "<img src='../image/wifipictorouge.png' width='50'>";  ?>
+                    <?= ($hebergement['fumeur'] == 1) ? "<img src='../image/fumeurpicto.png' width='50'>" : "<img src='../image/fumeurpictorouge.png' width='50'>";  ?>
+                    <?= ($hebergement['piscine'] == 1) ? "<img src='../image/piscinepicto.png' width='50'>" : "<img src='../image/piscinepictorouge.png' width='50'>";  ?>
+                </p>
 
-                    <p>Photo 1:
-                        <?= ($hebergement['photo1'] == null) ? "<img src='../image/photovide.png' width='50'" : "<img src='" . $Hebergement['photo1'] . "' width='200'>"; ?>
-                    </p>
-                    <p>Debut: <?= $periode['debut'] ?></p>
-                    <p>Fin: <?= $periode['fin'] ?></p>
-                    <p>
-                        <a href='index.php' class='btn btn-primary'>Retour<a>
-                                <a href="edit.php?id_hebergement=" <?= $hebergement['id_hebergement']; ?> class='btn btn-primary'>>Modifier</a>
-                    </p>
+                <p>Photo 1:
+                    <input type="hidden" id="id_periode" name="id_periode" value=<?= $hebergement['id_periode'] ?>>
+                    <?= ($hebergement['photo1'] == null) ? "<img src='../image/photovide.png' width='50'" : "<img src='" . $Hebergement['photo1'] . "' width='200'>"; ?>
+                </p>
+                <p>Debut: <?= $periode['debut'] ?></p>
+                <p>Fin: <?= $periode['fin'] ?></p>
+                <p>
+                    <a href='index.php' class='btn btn-primary'>Retour<a>
+                            <a href="edit.php?id_hebergement=<?= $hebergement['id_hebergement']; ?>" class='btn btn-primary'>Modifier</a>
+                </p>
             </section>
         </div>
     </main>
 </body>
 
-</html>$retVal = (condition) ? a : b ;
+</html>
