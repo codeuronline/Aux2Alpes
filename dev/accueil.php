df8060900d512debdc47aa2810c0ded36b83eb9a
PCGameJK <j.kasperski@codeur.online>
2022-01-22 19:45:56 +0100

    hello

--- dev/accueil.php
+++ dev/accueil.php
@@ -9,15 +9,25 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href=".\radio.css">
-    <link rel="stylesheet" href="">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="radio.css">
+    <link rel="stylesheet" href="test.css">
     <title>Accueil</title>
 
 </head>
 
 <body>
-
+    <div class="container">
+        <div class="a"><span>Gites Bonbons</span> </div>
+        <div class="b"><span>mosaique de gite</span></div>
+        <div class="c"><span>mosaique de gite</span></div>
+        <div class="d">formualaire</div>
+        <div class="e">coucou</div>
+        <div class="f">coucou</div>
+        <div class="g">hello</div>
+        <div class="h">world</div>
+    </div>
 </body>
 
 </html>
