cab5d5352f65032826c0e4f146a2149b60e155ba
codeuronline <j.kasperki@codeur.online>
2022-01-27 12:35:22 +0100

    validation

--- dev/details.php
+++ dev/details.php
@@ -35,10 +35,9 @@ if (isset($_GET['id_hebergement']) && !empty($_GET['id_hebergement'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="gitebonbon.css">
-    
+
 
     <title>Détails de l'hébergement</title>
 </head>
@@ -59,13 +58,8 @@ if (isset($_GET['id_hebergement']) && !empty($_GET['id_hebergement'])) {
                 <p>
                     <?= ($hebergement['animaux'] == "1") ? "<img src='../image/animauxpictorouge.png' width='50'>" : "<img src='../image/animauxpicto.png' width='50'>";  ?>
                     <?= ($hebergement['wifi'] == "1") ? "<img src='../image/wifipictorouge.png' width='50'>" : "<img src='../image/wifipicto.png' width='50'>";  ?>
-<<<<<<< HEAD
-                    <?= ($hebergement['fumeur'] == "1") ? "<img src='../image/fumeupictorouge.png' width='50'>" : "<img src='../image/fumeurpicto.png' width='50'>";  ?>
-                    <?= ($hebergement['piscine'] == "1") ? "<img src='../image/piscinepictoouge.png' width='50'>" : "<img src='../image/piscinepicto.png' width='50'>";  ?>
-=======
                     <?= ($hebergement['fumeur'] == "1") ? "<img src='../image/fumeurpictorouge.png' width='50'>" : "<img src='../image/fumeurpicto.png' width='50'>";  ?>
                     <?= ($hebergement['piscine'] == "1") ? "<img src='../image/piscinepictorouge.png' width='50'>" : "<img src='../image/piscinepicto.png' width='50'>";  ?>
->>>>>>> a97259ec0fb8d851035008bd43f4d072f5366a1f
                     <?= ($hebergement['taxi'] == "1") ? "<img src='../image/taxipictorouge.png' width='50'>" : "<img src='../image/taxipicto.png' width='50'>";  ?>
                     <?= ($hebergement['douche'] == "1") ? "<img src='../image/douchepictorouge.png' width='50'>" : "<img src='../image/douchepicto.png' width='50'>";  ?>
                 </p>
@@ -86,8 +80,7 @@ if (isset($_GET['id_hebergement']) && !empty($_GET['id_hebergement'])) {
                 <p>Fin: <?= $periode['fin']; ?></p>
                 <p>
                     <a href='index.php' class='btn btn-primary'>Retour<a>
-                            <a href="edit.php?id_hebergement=<?= $hebergement['id_hebergement']; ?>"
-                                class='btn btn-primary'>Modifier</a>
+                            <a href="edit.php?id_hebergement=<?= $hebergement['id_hebergement']; ?>" class='btn btn-primary'>Modifier</a>
                 </p>
             </section>
         </div>
