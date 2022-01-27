df3c72ec495f9bc01b8daced55a180c64f45601a
codeuronline <j.kasperki@codeur.online>
2022-01-27 12:15:50 +0100

    dev-back

--- dev/details.php
+++ dev/details.php
@@ -57,12 +57,12 @@ if (isset($_GET['id_hebergement']) && !empty($_GET['id_hebergement'])) {
                 <p>Couchage: <?= $hebergement['couchage'] ?></p>
                 <p>Sdb: <?= $hebergement['sdb'] ?></p>
                 <p>
-                    <?= ($hebergement['animaux'] == "1") ? "<img src='../image/animauxpicto.png' width='50'>" : "<img src='../image/animauxpictorouge.png' width='50'>";  ?>
-                    <?= ($hebergement['wifi'] == "1") ? "<img src='../image/wifipicto.png' width='50'>" : "<img src='../image/wifipictorouge.png' width='50'>";  ?>
-                    <?= ($hebergement['fumeur'] == "1") ? "<img src='../image/fumeurpicto.png' width='50'>" : "<img src='../image/fumeurpictorouge.png' width='50'>";  ?>
-                    <?= ($hebergement['piscine'] == "1") ? "<img src='../image/piscinepicto.png' width='50'>" : "<img src='../image/piscinepictorouge.png' width='50'>";  ?>
-                    <?= ($hebergement['taxi'] == "1") ? "<img src='../image/taxipicto.png' width='50'>" : "<img src='../image/taxipictorouge.png' width='50'>";  ?>
-                    <?= ($hebergement['douche'] == "1") ? "<img src='../image/douchepicto.png' width='50'>" : "<img src='../image/douchepictorouge.png' width='50'>";  ?>
+                    <?= ($hebergement['animaux'] == "1") ? "<img src='../image/animauxpictorouge.png' width='50'>" : "<img src='../image/animauxpicto.png' width='50'>";  ?>
+                    <?= ($hebergement['wifi'] == "1") ? "<img src='../image/wifipictorouge.png' width='50'>" : "<img src='../image/wifipicto.png' width='50'>";  ?>
+                    <?= ($hebergement['fumeur'] == "1") ? "<img src='../image/fumeupictorouge.png' width='50'>" : "<img src='../image/fumeurpicto.png' width='50'>";  ?>
+                    <?= ($hebergement['piscine'] == "1") ? "<img src='../image/piscinepictoouge.png' width='50'>" : "<img src='../image/piscinepicto.png' width='50'>";  ?>
+                    <?= ($hebergement['taxi'] == "1") ? "<img src='../image/taxipictorouge.png' width='50'>" : "<img src='../image/taxipicto.png' width='50'>";  ?>
+                    <?= ($hebergement['douche'] == "1") ? "<img src='../image/douchepictorouge.png' width='50'>" : "<img src='../image/douchepicto.png' width='50'>";  ?>
                 </p>
 
                 <p>Photo :
