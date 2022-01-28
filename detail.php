0f146cf68b1e976f6f50149bf52189e15427b713
Mika73100 <Michael-73@live.Fr>
2022-01-28 15:39:53 +0100

    peutetre

--- detail.php
+++ detail.php
@@ -1,14 +1,9 @@
 <?php
 
 session_start();
-require_once('dev/connect.php');
-require_once('dev/tools.php');
+require_once('toolsformika.php');
 
-$sql='SELECT * FROM `hebergement` WHERE `id_hebergement` = :id';
-    $query = $db->prepare($sql);
-    $query->bindValue(':id', $_POST['nom']);
-    $query->execute();
-    $hebergement = $query->fetch(PDO::FETCH_ASSOC);
+$hebergement = selectHebergementbyIdFull($_GET['id']);
 
 
 ?>
@@ -16,110 +11,81 @@ $sql='SELECT * FROM `hebergement` WHERE `id_hebergement` = :id';
 <html lang="fr">
 
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="gitebonbon.css">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="griddetail.css">
 </head>
 
-<header> 
-  <a href="inscription.php"><button class="btn-inscription-connexion" type=submit>Inscription</button></a>
-  <a href="connexion.php"><button class="btn-inscription-connexion" type=submit>Connexion</button></a>
-</header>
-
-<a href="index.php"><img class="logo1" src="image/logo.png" width="100px" height="100px" alt="Logo"></a>
+<!--<header> 
+    <a href="inscription.php"><button class="btn-inscription-connexion" type=submit>Inscription</button></a>
+    <a href="connexion.php"><button class="btn-inscription-connexion" type=submit>Connexion</button></a>
+</header>-->
 
 
 
-<div class="container4">
-  <div class="a">
-    <img src='<?php echo "dev/photo/".$hebergement['photo1']; ?>'></div>
-  <div class="b"><img src='<?php echo "dev/photo/".$hebergement['photo2']; ?>'></div>
-  <div class="c"><img src='<?php echo "dev/photo/".$hebergement['photo3']; ?>'></div>
-  <div class="d"><img src='<?php echo "dev/photo/".$hebergement['photo4']; ?>'></div>
-  <div class="e"><img src='<?php echo "dev/photo/".$hebergement['photo5']; ?>'></div>
-</div>
 
 <body>
-  <article>
-    <div id="slides">
+    <div class="container">
 
-    <?php
-    for ($i=1; $i <= 5; $i++) { 
-        $image=$hebergement['photo'.$i];
-      echo "<img src='$image'  alt=''>";  
-    }
-    ?>
+        <div class="logo">
+            <a href="index1.php"><img class="logo1" src="image/logo.png" width="100px" height="100px" alt="Logo"></a>
+        </div>
 
+        <div class="album">
+            <div class="photo1"><img src='<?php echo "dev/photo/" . $hebergement['photo1']; ?>'></div>
+            <div class="photo2"><img src='<?php echo "dev/photo/" . $hebergement['photo2']; ?>'></div>
+            <div class="photo3"><img src='<?php echo "dev/photo/" . $hebergement['photo3']; ?>'></div>
+            <div class="photo4"><img src='<?php echo "dev/photo/" . $hebergement['photo4']; ?>'></div>
+            <div class="photo5"><img src='<?php echo "dev/photo/" . $hebergement['photo5']; ?>'></div>
+        </div>
+
+        <div class="nom">
+            <h1><?php echo $hebergement['nom']; ?><br></h1>
+
+            <div class="disponible"><img src=image/calendrierpicto.png height="50"
+                    width="50"><?php echo $hebergement['debut']; ?>/<?php echo $hebergement['fin']; ?></div>
+            <div class="couchage"><img src=image/litpicto.png height="50" width="50">couchage:
+                <?php echo $hebergement['couchage']; ?></div>
+            <div class="prix"><?php echo $hebergement['prix']; ?><img src=image/europicto.png height="50" width="50">
+            </div>
+        </div>
+
+        <div class="description">Description: <?php echo $hebergement['description']; ?></div>
+
+        <div class="option">
+            <?= ($hebergement['animaux'] == "1") ? "<img src='image/animauxpictorouge.png' width='50'>" : "<img src='image/animauxpicto.png' width='50'>"; ?>
+            <?= ($hebergement['wifi'] == "1") ? "<img src='image/wifipictorouge.png' width='50'>" : "<img src='image/wifipicto.png' width='50'>"; ?>
+            <?= ($hebergement['fumeur'] == "1") ? "<img src='image/fumeurpictorouge.png' width='50'>" : "<img src='image/fumeurpicto.png' width='50'>"; ?>
+            <?= ($hebergement['piscine'] == "1") ? "<img src='image/piscinepictorouge.png' width='50'>" : "<img src='image/piscinepicto.png' width='50'>"; ?>
+            <?= ($hebergement['taxi'] == "1") ? "<img src='image/taxipictorouge.png' width='50'>" : "<img src='image/taxipicto.png' width='50'>"; ?>
+            <?= ($hebergement['douche'] == "1") ? "<img src='image/douchepictorouge.png' width='50'>" : "<img src='image/douchepicto.png' width='50'>"; ?>
+        </div>
+        <div class='formulaire'>
+
+            <form method="POST" action="booking.php">
+
+                <?php //ici pour demander a l utilisateur de s'enregristré  
+
+                $_SESSION['id_user'] = 2;
+
+                if (!(@$_SESSION['id_user'])) {
+
+                    echo  "<label><input placeholder='prenom' type='text'></label><br>";
+                    echo  "<label><input placeholder='nom' type='text'></label><br>";
+                    echo  "<label><input placeholder='mail' type='email'></label><br>";
+                }
+                echo "<input type='hidden' name='personne' value='" . $hebergement['couchage'] . "'>";
+                echo "<input type='hidden' name='id_hebergement' value='" . $hebergement['id_hebergement'] . "'>"; ?>
+                <input type='date' value='<?= date('Y-m-d') ?>' name='debut'>
+                <input type='date' value='' name='fin'><br>
+                <button class=btn-inscription-connexion type='submit' class='bg-text-light'>Reservation</button>
+
+            </form>
+        </div>
     </div>
-  </article>
-
-                                <div class="g">
-                                <!--------------------Description de l'hotel ---------------->
-                                
-                                <img src=image/calendrierpicto.png height="50" width="50">
-                                <div class="groupe"><input type="date" name="Arriver" id="Arriver" required><input type="date" name="Retour" id="Retour" required></div></button>
-                                
-                                <h2>
-                                <?php echo $hebergement['nom'];?><br>
-                                </h2>
-
-                                <div>Description: <?php echo $hebergement['description'];?><br>
-                                couchage: <?php echo $hebergement['couchage'];?><br>
-                                
-                                </div>
-
-                                
-                                
-                              
-
-                                
-                            </div>
-                            </div>
-
-                            
-
-                          <div><?php //ici pour demander a l utilisateur de s'enregristré  
-                            
-                            $_SESSION['id_user']=2;
-                            
-                            if (!(@$_SESSION['id_user'])){
-                              
-                              echo  "<label><input placeholder='prenom' type='text'></label><br>";
-                              echo  "<label><input placeholder='nom' type='text'></label><br>";
-                              echo  "<label><input placeholder='mail' type='email'></label><br>";
-                            }
-                              echo "<input type='hidden' name='couchage' value='".$hebergement['couchage']."'>";
-                              echo "<input type='hidden' name='id_hebergement' value='".$hebergement['id_hebergement']."'>";
-                              
-                              
 
 
-                            
-                            //requet sur periode & jour
-                            $sql="SELECT date_jour FROM jour WHERE id_periode=".$hebergement['id_periode'];
-                            $query=$db->prepare($sql);
-                            $query->execute();
-
-                            $result=$query->fetch(PDO::FETCH_ASSOC);
-                            
-                            
-                            echo "<input type='date' value='".$result['date_jour']."' name='debut'>";
-                            
-                            echo "<input type='date' value='' name='fin'>";
-                              
-
-                          ?></div>
-
-
-                          <button class=btn-recherche type='submit' class='bg-text-light'>Reservation</button>
-
-                          </form>
-                      </div>
-                  </div>
-              </div>
-          </div>
-      </div>
 </body>
 
 
