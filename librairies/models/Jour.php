6d8cd650358c0751684bac4194ad77a0c0ef82f9
PCGameJK <j.kasperski@codeur.online>
2022-02-02 23:09:54 +0100

    constructor periode2

---
+++ librairies/models/Jour.php
@@ -0,0 +1,52 @@
+<?php
+require_once('../librairies/database.php');
+require_once('librairies/database.php');
+require_once('librairies/utils.php');
+class Jour
+{
+
+    public function __construct($id, $debut, $fin)
+    {
+    }
+
+    public function add($id, $debut, $fin)
+    {
+        $pdo = getPDO();
+        for ($i = 0; $i <= dateDiff($debut, $fin); $i++) {
+            $value = date("Y-m-d", strtotime($debut . "+ $i days"));
+            $compteur = $i + 1;
+            $sql = 'INSERT INTO jour(id_periode,date_jour,periode_jour,etat) VALUES (:id, :date_jour,:periode_jour, 0)';
+            $query = $pdo->prepare($sql);
+            $query->bindValue(":id", $id);
+            $query->bindValue(":date_jour", $value);
+            $query->bindValue(":periode_jour", $compteur);
+            $query->execute();
+        }
+    }
+
+    function updateReserved($id, $debut, $fin): void
+    {
+        $pdo = getPDO();
+        $indice['intervalle'] = dateDiff($debut, $fin);
+        for ($i = 0; $i <= $indice['intervalle']; $i++) {
+            $value = date("Y-m-d", strtotime($debut . "+ $i days"));
+            $compteur = $i + 1;
+            //echo "valeur à reserver:" . $value . "<br>";
+            $sql = 'UPDATE `jour` SET etat=1 WHERE id_periode=:id_periode AND date_jour=:date_jour';
+            $query = $pdo->prepare($sql);
+            $query->bindValue(":id_periode", $id);
+            $query->bindValue(":date_jour", $value);
+            $query->execute();
+        }
+    }
+    public function selectFree($id): array
+    {
+        $pdo = getPDO();
+
+        $sql = "SELECT * FROM `jour` WHERE id_periode=:id AND etat=0";
+        $query = $pdo->prepare($sql);
+        $query->bindValue(':id', $id);
+        $query->execute();
+        return $query->fetchAll();
+    }
+}
